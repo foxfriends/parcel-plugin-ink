dba2a645fc2af91d0920761fb2ad6186cca42713
Cameron Eldridge <cameldridge@gmail.com>
2020-08-27 23:49:43 -0400

    Try again

--- src/InkAsset.js
+++ src/InkAsset.js
@@ -4,12 +4,15 @@ const { Asset } = require('parcel-bundler');
 class InkAsset extends Asset {
   type = 'js';
 
-  parse() {
+  async parse() {
+    console.log(this.name);
     return inklecate({ inputFilepath: this.name });
   }
 
-  generate() {
-    return `module.exports = ${JSON.stringify(this.ast.storyContent, null, 2)}`;
+  async generate() {
+    const code = `module.exports = ${JSON.stringify(this.ast.storyContent, null, 2)}`;
+    console.log(code);
+    return code;
   }
 }
 
