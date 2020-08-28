0e5227e0e8bdf9857c88f24f5cb9181e4ab31dfa
Cameron Eldridge <cameldridge@gmail.com>
2020-08-28 00:05:01 -0400

    How about this

--- src/InkAsset.js
+++ src/InkAsset.js
@@ -2,17 +2,18 @@ const { inklecate } = require('inklecate');
 const { Asset } = require('parcel-bundler');
 
 class InkAsset extends Asset {
-  type = 'js';
+  constructor(...args) {
+    super(...args);
+    this.type = 'js';
+  }
 
   async parse() {
-    console.log(this.name);
     return inklecate({ inputFilepath: this.name });
   }
 
   async generate() {
-    const code = `module.exports = ${JSON.stringify(this.ast.storyContent, null, 2)}`;
-    console.log(code);
-    return code;
+    const js = `module.exports = ${JSON.stringify(this.ast.storyContent, null, 2)}`;
+    return { js };
   }
 }
 
