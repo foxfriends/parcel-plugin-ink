92e09abdda13ee99e2b470169f2ebbffde4a1693
Cameron Eldridge <cameldridge@gmail.com>
2020-09-06 22:12:18 -0400

    Dependencies

--- src/InkAsset.js
+++ src/InkAsset.js
@@ -1,4 +1,6 @@
 const { inklecate } = require('inklecate');
+const { readFile } = require('fs').promises;
+const { dirname, join } = require('path');
 const { Asset } = require('parcel-bundler');
 
 class InkAsset extends Asset {
@@ -9,6 +11,26 @@ class InkAsset extends Asset {
 
   parse() {
     return inklecate({ inputFilepath: this.name });
+  }
+
+  async collectDependencies() {
+    const extractPaths = (base, text) => text
+      .split('\n')
+      .filter((line) => /^\s*INCLUDE\b/.test(line))
+      .map((line) => line.slice(line.indexOf('INCLUDE') + 7).trim())
+      .map((file) => join(base, file));
+    const dependencies = new Set();
+    const paths = extractPaths(dirname(this.name), this.ast.text);
+    while (paths.length) {
+      const dependency = paths.pop();
+      if (dependencies.has(dependency)) continue;
+      dependencies.add(dependency);
+      const text = await readFile(dependency);
+      paths.push(...extractPaths(dirname(dependency), text.toString('utf8')));
+    }
+    for (const dependency of dependencies) {
+      this.addDependency(dependency, { includedInParent: true });
+    }
   }
 
   generate() {
